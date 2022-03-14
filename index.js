5b97ac8acfe5c9530bb3ac5b38b2905677309788
Joabe costa de almeida oliveira <85427303+Joabesued@users.noreply.github.com>
2022-03-14 18:18:51 -0300

    100%

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ let elementoBtn = document.querySelector('#alterar')
 elementoBtn.addEventListener('click', () =>{
     
     if(elementoBtn.value == 'primeiro'){
-        elementoImg.src = './assets/img/amianto.jpg' 
+        elementoImg.src = 'amianto.jpg' 
         elementoMusica.innerText = 'Amianto' 
         elementoAutor.innerText = 'SUPERCOMBO' 
         elementoBtn.value = 'segundo' 
     } else if(elementoBtn.value == 'segundo') {
-        elementoImg.src = './assets/img/vitorkley6.jpg'  
+        elementoImg.src = 'vitorkley6.jpg'  
         elementoMusica.innerText = 'Duvida' 
         elementoAutor.innerText = 'Vitor Kley ft. Jão' 
         elementoBtn.value = 'terceiro' 
     } else { 
-        elementoImg.src = './assets/img/dinheiro.jpg' 
+        elementoImg.src = 'dinheiro.jpg' 
         elementoMusica.innerText = 'Faça dinheiro, se mantenha vivo.' 
         elementoAutor.innerText = 'LEALL' 
         elementoBtn.value = 'primeiro' 
